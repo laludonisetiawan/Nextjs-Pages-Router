4d0b8e79d7473624ff74512a2ebd3a5a96040bda
laludonisetiawan <laludonisetiawan@gmail.com>
2023-11-27 15:24:52 +0800

    2.Page & Layout Update Optional slug [[...slug]].tsx

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -1,12 +1,12 @@
 import { useRouter } from 'next/router';
-const IdProduct = () => {
+const DetailProduct = () => {
   const { query } = useRouter();
   return (
     <div>
       <h1>ini adalah id dari produk</h1>
-      <h3>Product: {query.id} </h3>
+      <h3>Product: {query.product} </h3>
     </div>
   );
 };
 
-export default IdProduct;
+export default DetailProduct;
