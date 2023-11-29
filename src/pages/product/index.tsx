672604024945d0c387896529718c1a9cd326cd4d
laludonisetiawan <laludonisetiawan@gmail.com>
2023-11-30 02:24:06 +0800

    12. Middleware

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -5,13 +5,8 @@ import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
 const ProductPage = () => {
-  const [isLogin, setIsLogin] = useState(true);
   const { push } = useRouter();
-  useEffect(() => {
-    if (!isLogin) {
-      push('/auth/login');
-    }
-  }, []);
+  
 
   const { data, error, isLoading } = useSWR('/api/product', fetcher);
 
