8ec1f707a6778c8e4566d6a68c7e6b81607d3b3c
laludonisetiawan <laludonisetiawan@gmail.com>
2023-12-02 16:51:55 +0800

    13. Setup Next-Auth with jwt,NextAuth.js, middleware

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,13 +1,26 @@
-import style from './Navbar.module.css'
+import { signIn, signOut, useSession } from 'next-auth/react';
+import style from './Navbar.module.css';
 
 const Navbar = () => {
+  const { data }: any = useSession();
+  console.log(data);
   return (
     <div className={style.navbar}>
+      <div className="big">Navbar</div>
       <div>
-      Navbar
+        {data && data.user.fullname}{' '}
+        {data ? (
+          <button className={style.button} onClick={() => signOut()}>
+            Sign out
+          </button>
+        ) : (
+          <button className={style.button} onClick={() => signIn()}>
+            Sign in
+          </button>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
